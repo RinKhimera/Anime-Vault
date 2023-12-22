1d774412fcda0c25d12a6f55715039af3f357d99
Samuel Pokam <dixiades@gmail.com>
2023-12-22 10:27:43 +0100

    shikimori api endpoint impl

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next"
+import { DM_Sans } from "next/font/google"
+
+import Footer from "@/components/Footer"
+import Hero from "@/components/Hero"
+
+import "./globals.css"
+
+const dmSans = DM_Sans({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Anime Vault",
+  description: "Your favorite anime, all in one place.",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={dmSans.className}>
+        <main className="max-w-7xl mx-auto bg-[#0F1117]">
+          <Hero />
+          {children}
+          <Footer />
+        </main>
+      </body>
+    </html>
+  )
+}
